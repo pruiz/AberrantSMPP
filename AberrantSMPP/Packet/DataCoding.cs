536287d28133fd857fc36044981fccd4d4b9e25a
Pablo Ruiz Garcia <pablo.ruiz@gmail.com>
2011-08-05 19:07:01 +0200

    Renamed DataCodingType to DataCoding and extracted from Pdu.cs so it no more is an embedded type.

---
+++ AberrantSMPP/Packet/DataCoding.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AberrantSMPP.Packet
+{
+	/// <summary>
+	/// Enumerates the data coding types.
+	/// </summary>
+	public enum DataCoding : byte
+	{
+		/// <summary>
+		/// SMSCDefault
+		/// </summary>
+		SMSCDefault = 0x00,
+		/// <summary>
+		/// IA5_ASCII
+		/// </summary>
+		IA5_ASCII = 0x01,
+		/// <summary>
+		/// OctetUnspecifiedB
+		/// </summary>
+		OctetUnspecifiedB = 0x02,
+		/// <summary>
+		/// Latin1
+		/// </summary>
+		Latin1 = 0x03,
+		/// <summary>
+		/// OctetUnspecifiedA
+		/// </summary>
+		OctetUnspecifiedA = 0x04,
+		/// <summary>
+		/// JIS
+		/// </summary>
+		JIS = 0x05,
+		/// <summary>
+		/// Cyrillic
+		/// </summary>
+		Cyrillic = 0x06,
+		/// <summary>
+		/// Latin_Hebrew
+		/// </summary>
+		Latin_Hebrew = 0x07,
+		/// <summary>
+		/// Pictogram
+		/// </summary>
+		Pictogram = 0x09,
+		/// <summary>
+		/// MusicCodes
+		/// </summary>
+		MusicCodes = 0x0A,
+		/// <summary>
+		/// ExtendedKanjiJIS
+		/// </summary>
+		ExtendedKanjiJIS = 0x0D,
+		/// <summary>
+		/// KS_C
+		/// </summary>
+		KS_C = 0x0E
+	}
+}
--- AberrantSMPP/Packet/Pdu.cs
+++ AberrantSMPP/Packet/Pdu.cs
@@ -1041,62 +1041,7 @@ namespace AberrantSMPP.Packet
 			/// </summary>
 			OnFailure = 0x02
 		}
-		
-		/// <summary>
-		/// Enumerates the data coding types.
-		/// </summary>
-		public enum DataCodingType : byte
-		{
-			/// <summary>
-			/// SMSCDefault
-			/// </summary>
-			SMSCDefault = 0x00,
-			/// <summary>
-			/// IA5_ASCII
-			/// </summary>
-			IA5_ASCII = 0x01,
-			/// <summary>
-			/// OctetUnspecifiedB
-			/// </summary>
-			OctetUnspecifiedB = 0x02,
-			/// <summary>
-			/// Latin1
-			/// </summary>
-			Latin1 = 0x03,
-			/// <summary>
-			/// OctetUnspecifiedA
-			/// </summary>
-			OctetUnspecifiedA = 0x04,
-			/// <summary>
-			/// JIS
-			/// </summary>
-			JIS = 0x05,
-			/// <summary>
-			/// Cyrillic
-			/// </summary>
-			Cyrillic = 0x06,
-			/// <summary>
-			/// Latin_Hebrew
-			/// </summary>
-			Latin_Hebrew = 0x07,
-			/// <summary>
-			/// Pictogram
-			/// </summary>
-			Pictogram = 0x09,
-			/// <summary>
-			/// MusicCodes
-			/// </summary>
-			MusicCodes = 0x0A,
-			/// <summary>
-			/// ExtendedKanjiJIS
-			/// </summary>
-			ExtendedKanjiJIS = 0x0D,
-			/// <summary>
-			/// KS_C
-			/// </summary>
-			KS_C = 0x0E
-		}
-		
+				
 		/// <summary>
 		/// Enumerates the privacy indicator types.
 		/// </summary>
