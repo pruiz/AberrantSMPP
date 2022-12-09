cb9299bf436948bacf1ebd704fac4f64663a222b
Iker Celorrio <icelorrio@evicertia.com>
2022-12-09 11:11:05 +0100

    fix Disconnect() deadlock

--- AberrantSMPP/RequestQueue.cs
+++ AberrantSMPP/RequestQueue.cs
@@ -1,73 +1,69 @@
 ﻿using System;
 using System.Collections.Specialized;
-using System.Threading;
 using System.Runtime.Caching;
 using System.Runtime.Caching.Configuration;
-
-
+using System.Threading;
 
 using AberrantSMPP.Packet.Request;
-using AberrantSMPP.Packet.Response;
 
 namespace AberrantSMPP
 {
-    internal class RequestQueue
-    {
-        private static readonly global::Common.Logging.ILog _log = global::Common.Logging.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+	internal class RequestQueue
+	{
+		private static readonly global::Common.Logging.ILog _log = global::Common.Logging.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        private MemoryCache _cache;
+		private MemoryCache _cache;
 		private readonly CacheItemPolicy _cachePolicy;
-        private readonly NameValueCollection _cacheConfig;
+		private readonly NameValueCollection _cacheConfig;
 		private readonly string _name;
-        private readonly bool _throwAddExistingSequence;
+		private readonly bool _throwAddExistingSequence;
 
-        public RequestQueue(string name, TimeSpan requestExpiration, bool throwAddExistingSequence, int cacheMemoryLimitMegabytes)
-        {
-            _name = name;
-            _throwAddExistingSequence = throwAddExistingSequence;
+		public RequestQueue(string name, TimeSpan requestExpiration, bool throwAddExistingSequence, int cacheMemoryLimitMegabytes)
+		{
+			_name = name;
+			_throwAddExistingSequence = throwAddExistingSequence;
 			_cachePolicy = new CacheItemPolicy()
-            {
-                // XXX: Adds 1 second of gace period to expiration.
-                SlidingExpiration = requestExpiration + TimeSpan.FromSeconds(1),
-                RemovedCallback = (args) =>
-                {
-                    if (args.RemovedReason != CacheEntryRemovedReason.Removed)
-                        _log.WarnFormat("Request #{0} has been {1} from cache.", args.CacheItem.Key, args.RemovedReason);
-                },
-            };
+			{
+				// XXX: Adds 1 second of gace period to expiration.
+				SlidingExpiration = requestExpiration + TimeSpan.FromSeconds(1),
+				RemovedCallback = (args) => {
+					if (args.RemovedReason != CacheEntryRemovedReason.Removed)
+						_log.WarnFormat("Request #{0} has been {1} from cache.", args.CacheItem.Key, args.RemovedReason);
+				},
+			};
 			_cacheConfig = new NameValueCollection(StringComparer.OrdinalIgnoreCase) {
 				{ nameof(MemoryCacheElement.CacheMemoryLimitMegabytes), cacheMemoryLimitMegabytes.ToString() }
 			};
 			_cache = new MemoryCache(_name, _cacheConfig, true);
-        }
+		}
 
-        private static string BuildKey(uint sequence) => sequence.ToString("x");
+		private static string BuildKey(uint sequence) => sequence.ToString("x");
 
 		public bool Add(uint sequence, SmppRequest request)
-        {
-            if (_throwAddExistingSequence && _cache.Contains(BuildKey(sequence)))
-                throw new ArgumentException("A request with the same sequence has already been added.");
+		{
+			if (_throwAddExistingSequence && _cache.Contains(BuildKey(sequence)))
+				throw new ArgumentException("A request with the same sequence has already been added.");
 
-            _cache.Set(BuildKey(sequence), request, _cachePolicy);
-            return true;
-        }
+			_cache.Set(BuildKey(sequence), request, _cachePolicy);
+			return true;
+		}
 
-        public bool Remove(uint sequence, out SmppRequest result)
-        {
-            result = _cache.Remove(BuildKey(sequence)) as SmppRequest;
-            return result != null;
-        }
+		public bool Remove(uint sequence, out SmppRequest result)
+		{
+			result = _cache.Remove(BuildKey(sequence)) as SmppRequest;
+			return result != null;
+		}
 
-        public void Clear()
-        {
-            var @new = new MemoryCache(_name, _cacheConfig, true);
+		public void Clear()
+		{
+			var @new = new MemoryCache(_name, _cacheConfig, true);
 			var old = Interlocked.Exchange(ref _cache, @new);
-            foreach( var cacheItemKvp in old)
-            {
-                var request = cacheItemKvp.Value as SmppRequest;
-                request?.CancelResponse();
-            }
+			foreach (var cacheItemKvp in old)
+			{
+				var request = cacheItemKvp.Value as SmppRequest;
+				request?.CancelResponse();
+			}
 			old.Dispose();
-        }
-    }
+		}
+	}
 }
--- AberrantSMPP/SMPPClient.EnquireLinkHandler.cs
+++ AberrantSMPP/SMPPClient.EnquireLinkHandler.cs
@@ -1,43 +1,44 @@
 ﻿using System;
-using Dawn;
-
-using DotNetty.Handlers.Timeout;
-using DotNetty.Transport.Channels;
-using DotNetty.Common.Utilities;
 
 using AberrantSMPP.Packet.Request;
 
+using Dawn;
+
+using DotNetty.Common.Utilities;
+using DotNetty.Handlers.Timeout;
+using DotNetty.Transport.Channels;
+
 namespace AberrantSMPP
 {
-    public partial class SMPPClient
-    {
-        private class EnquireLinkHandler : IdleStateHandler
-        {
-            private readonly SMPPClient _client;
+	public partial class SMPPClient
+	{
+		private class EnquireLinkHandler : IdleStateHandler
+		{
+			private readonly SMPPClient _client;
 
-            public EnquireLinkHandler(SMPPClient owner)
-                : base(false, owner.EnquireLinkInterval, TimeSpan.Zero, TimeSpan.Zero)
-            {
+			public EnquireLinkHandler(SMPPClient owner)
+				: base(false, owner.EnquireLinkInterval, TimeSpan.Zero, TimeSpan.Zero)
+			{
 				base.EnsureNotSharable();
 
 				_client = Guard.Argument(owner, nameof(owner)).NotNull();
-            }
+			}
 
-            protected override void ChannelIdle(IChannelHandlerContext context, IdleStateEvent stateEvent)
-            {
-                Guard.Argument(stateEvent).NotNull();
+			protected override void ChannelIdle(IChannelHandlerContext context, IdleStateEvent stateEvent)
+			{
+				Guard.Argument(stateEvent).NotNull();
 
-                if (_client.EnquireLinkInterval <= TimeSpan.Zero)
-                    return;
+				if (_client.EnquireLinkInterval <= TimeSpan.Zero)
+					return;
 
-                // SMPPv5.0 -- 2.4. Operation Matrix
-                // Once connected, enquire request can be sent on any state
-                if (_client.State < States.Connected)
-                    return;
+				// SMPPv5.0 -- 2.4. Operation Matrix
+				// Once connected, enquire request can be sent on any state
+				if (_client.State < States.Connected)
+					return;
 
-                ReferenceCountUtil.SafeRelease(stateEvent);
-                context.WriteAndFlushAsync(new SmppEnquireLink());
-            }
-        }
-    }
+				ReferenceCountUtil.SafeRelease(stateEvent);
+				context.WriteAndFlushAsync(new SmppEnquireLink());
+			}
+		}
+	}
 }
